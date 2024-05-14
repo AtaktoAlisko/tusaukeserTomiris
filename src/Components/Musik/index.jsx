4c18fb008fb409507569c6ccb95b689dbddcaf6d
Алибек Мурат <alibekmurat@MacBook-Pro-Alibek.local>
2024-05-14 06:02:56 +0500

    Ppp

--- src/Components/Musik/index.jsx
+++ src/Components/Musik/index.jsx
@@ -4,24 +4,9 @@
 import { useState, useRef, useEffect } from "react";
 import styles from "./Musik.module.scss";
 import proigr from "../../../public/images/proigriv.png";
-import muz from "../../assets/muzik.mp3";
 import Image from "next/image";
 
 export default function Musik() {
-  const [audio] = useState(new Audio(muz));
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(event) {
-    if (isPlaying) {
-      audio.pause();
-    } else {
-      audio.play();
-    }
-
-    setIsPlaying(!isPlaying);
-    event.target.classList.toggle("rotate-image");
-  }
-
   return (
     <button
       className={`${styles.muzik} ${isPlaying ? "animate-spin" : ""}`}
